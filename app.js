7b29b485a21de243c90faa5757c6d94d2a6eda09
xuhexin <xuhexin@baidu.com>
2025-07-09 11:21:18 +0800

    GPT-XIN-SERVER【feat】实现文生图接口

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const chatRouter = require('./routes/chat');
+const pictureRouter = require('./routes/picture');
 
 const port = 3002;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', chatRouter);
+app.use('/', pictureRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ routes/picture.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const OpenAi = require('openai');
+require('dotenv').config();
+const cors = require('cors'); // 引入 cors
+
+const client = new OpenAi({
+  apiKey: process.env.OPENAI_API_KEY, // 使用环境变量加载 API 密钥
+  baseURL: 'https://api.chatanywhere.tech/v1',
+})
+
+router.use(cors());
+
+/* GET users listing. */
+router.post('/picture', async function(req, res) {
+  const { message } = req.body;
+  console.log(message);
+  const imageRes = await client.images.generate({
+    prompt: message,
+    n: 1,
+    size: '1024x1024',
+    model: 'dall-e-3',
+  });
+  console.log(imageRes.data[0])
+  res.json({
+    code: 0,
+    msg: '生成成功',
+    data: imageRes.data[0].url,
+    cardId: Date.now()
+  });
+
+  res.end();
+});
+
+module.exports = router;
